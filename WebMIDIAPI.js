14a309b587218c611b3e2e36fd885d7ee1d38417
Marcos Caceres <marcosscaceres@gmail.com>
2012-12-16 00:25:29 +0000

    fixed bug for when plugin is not available, added .toJSON support.

--- WebMIDIAPI.js
+++ WebMIDIAPI.js
@@ -1,7 +1,5 @@
-/*
-TODO: File bug about missing strigifier, which Chris had defined.
-*/
-(function(global, exports, perf) {
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:false, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
+(function (global, exports, perf) {
     'use strict';
     var midiIO,
         debug = false;
@@ -41,27 +39,14 @@ TODO: File bug about missing strigifier, which Chris had defined.
     See also: http://jazz-soft.net/doc/Jazz-Plugin/reference.html
     */
     function JazzPlugin() {
-        var self = this,
+        var dispatcher = document.createElement('x-eventDispatcher'),
             plugin = loadPlugin(),
-            timeZero = 0,
-            perfTimeZero = perf.now(),
             permitted = null,
             inputPorts = [],
             outputPorts = [],
-            dispatcher = document.createElement('x-eventDispatcher'),
             interfaces = {
-                timeZero: {
-                    get: function() {
-                        return timeZero;
-                    }
-                },
-                perfTimeZero: {
-                    get: function() {
-                        return perfTimeZero;
-                    }
-                },
                 time: {
-                    get: function() {
+                    get: function () {
                         return plugin.Time();
                     }
                 },
@@ -69,23 +54,23 @@ TODO: File bug about missing strigifier, which Chris had defined.
                     value: checkPermission
                 },
                 midiInList: {
-                    get: function() {
+                    get: function () {
                         return inputPorts;
                     }
                 },
                 midiOutList: {
-                    get: function() {
+                    get: function () {
                         return outputPorts;
                     }
                 },
                 midiInOpen: {
-                    value: function(name, callback) {
+                    value: function (name, callback) {
                         //TODO: check if authorized
                         plugin.MidiInOpen(name, callback);
                     }
                 },
                 midiOutLong: {
-                    value: function(data, name) {
+                    value: function (data, name) {
                         if (plugin.MidiOutOpen(name) === name) {
                             //plugin.MidiOut(0x90,60,100);
                             plugin.MidiOutLong(data);
@@ -93,11 +78,6 @@ TODO: File bug about missing strigifier, which Chris had defined.
                     }
                 }
             };
-        //once we are allowed, lets get the ports list
-        dispatcher.addEventListener('allowed', function(e) {
-            inputPorts = createPorts(e.data.inputPorts, 'input');
-            outputPorts = createPorts(e.data.outputPorts, 'output');
-        });
 
         function createPorts(list, type) {
             var ports = [];
@@ -108,15 +88,16 @@ TODO: File bug about missing strigifier, which Chris had defined.
             return ports;
         }
 
+
         function checkPermission(successCB, errorCB) {
             //going to ask permission for the first time
             if (permitted === null) {
                 requestPermission(plugin);
-                dispatcher.addEventListener('allowed', function() {
+                dispatcher.addEventListener('allowed', function () {
                     successCB(new MIDIAccess());
                 });
                 if (errorCB && typeof errorCB === 'function') {
-                    dispatcher.addEventListener('denied', function(e) {
+                    dispatcher.addEventListener('denied', function (e) {
                         errorCB(new Error(e.data));
                     });
                 }
@@ -131,7 +112,6 @@ TODO: File bug about missing strigifier, which Chris had defined.
         //loads the Jazz plugin by creating an <object>
         function loadPlugin() {
             var plugin,
-            msg = 'Failed to initialize. Maybe missing Jazz plugin? http://jazz-soft.net/',
                 elemId = '_Jazz' + Math.random(),
                 objElem = document.createElement('object'),
                 fallbackObj = objElem.cloneNode();
@@ -144,7 +124,7 @@ TODO: File bug about missing strigifier, which Chris had defined.
             plugin = (fallbackObj.isJazz === true) ? fallbackObj : objElem;
             if (!plugin instanceof window.HTMLObjectElement || !(plugin.isJazz)) {
                 var e = new window.CustomEvent('error');
-                e.data('NotSupportedError');
+                e.data = new Error('NotSupportedError');
                 dispatcher.dispatchEvent(e);
                 return null;
             }
@@ -161,12 +141,15 @@ TODO: File bug about missing strigifier, which Chris had defined.
                 id = 'dialog_' + (Math.round(Math.random() * 1000000) + 10000),
                 markup = '',
                 css = '';
+            if(!(plugin)){
+                throw "Jazz plugin was not Initialized. Did you install it?";
+            }
             css += '#' + id + '{ ' + 'width: 60%; ' + 'box-shadow: 0px 2px 20px black; z-index: 1000;' + ' left: 20%; background-color: #aaa;' + ' padding: 3em;' + '} ' + '.hidden{ top: -' + Math.round(window.innerHeight) + 'px; -webkit-transition: all .2s ease-in;}' + '.show{top: 0px; -webkit-transition: all .2s ease-out;}';
             style.innerHTML = css;
             div.id = id;
             markup += '<div>' + '<h1>♫ MIDI ♫</h1>' + '<p>' + window.location.host + ' wants to access your MIDI devices.</p>' + '<p><strong>Input Devices:</strong> ' + plugin.MidiInList().join(', ') + '.</p>' + '<p><strong>Output Devices:</strong> ' + plugin.MidiOutList().join(', ') + '.</p>' + '</div>';
             okButton.innerHTML = 'Allow';
-            okButton.onclick = function() {
+            okButton.onclick = function () {
                 var e = new window.CustomEvent('allowed');
                 div.className = 'hidden';
                 permitted = true;
@@ -177,7 +160,7 @@ TODO: File bug about missing strigifier, which Chris had defined.
                 dispatcher.dispatchEvent(e);
             };
             cancelButton.innerHTML = 'Cancel';
-            cancelButton.onclick = function() {
+            cancelButton.onclick = function () {
                 var e = new window.CustomEvent('denied');
                 e.data = 'SecurityError';
                 dispatcher.dispatchEvent(e);
@@ -191,11 +174,11 @@ TODO: File bug about missing strigifier, which Chris had defined.
             document.body.appendChild(div);
             document.head.appendChild(style);
             div.style.position = 'fixed';
-            setTimeout(function() {
+            setTimeout(function () {
                 div.className = 'show';
             }, 100);
             if (debug) {
-                setTimeout(function() {
+                setTimeout(function () {
                     okButton.click();
                 }, 500);
             }
@@ -211,17 +194,17 @@ TODO: File bug about missing strigifier, which Chris had defined.
             var e = new window.CustomEvent('message'),
                 interfaces = {
                     timestamp: {
-                        get: function() {
+                        get: function () {
                             return timestamp;
                         }
                     },
                     data: {
-                        get: function() {
+                        get: function () {
                             return data;
                         }
                     },
                     port: {
-                        get: function() {
+                        get: function () {
                             return port;
                         }
                     }
@@ -231,12 +214,13 @@ TODO: File bug about missing strigifier, which Chris had defined.
         }
         /*
         interface MIDIPort : EventListener {
-        readonly attribute DOMString    id;
-        readonly attribute DOMString?   manufacturer;
-        readonly attribute DOMString?   name;
-        readonly attribute MIDIPortType type;
-        readonly attribute DOMString?   version;
-                 attribute EventHandler? onmessage;
+            readonly attribute DOMString    id;
+            readonly attribute DOMString?   manufacturer;
+            readonly attribute DOMString?   name;
+            readonly attribute MIDIPortType type;
+            readonly attribute DOMString?   version;
+                     attribute EventHandler? onmessage;
+            serializer = {id, manufacturer, name, type, version};
         }
         */
         function MIDIPort(name, type) {
@@ -247,49 +231,49 @@ TODO: File bug about missing strigifier, which Chris had defined.
                 manufacturer = null,
                 version = null,
                 eventHandler = null,
-                messageCallback = (type === 'input') ? function(timestamp, data) {
+                messageCallback = (type === 'input') ? function (timestamp, data) {
                     var e = new MIDIEvent(timestamp, data, self);
                     dispatcher.dispatchEvent(e);
                 } : null,
                 interfaces = {
                     id: {
-                        get: function() {
+                        get: function () {
                             return id;
                         }
                     },
                     type: {
-                        get: function() {
+                        get: function () {
                             return type;
                         }
                     },
                     version: {
-                        get: function() {
+                        get: function () {
                             return version;
                         }
                     },
                     name: {
-                        get: function() {
+                        get: function () {
                             return deviceName;
                         }
                     },
                     //EventListener - can't extend type so need to implement:(
                     addEventListener: {
-                        value: function(type, listener, useCapture) {
+                        value: function (type, listener, useCapture) {
                             dispatcher.addEventListener(type, listener, useCapture);
                         }
                     },
                     removeEventListener: {
-                        value: function(type, listener, useCapture) {
+                        value: function (type, listener, useCapture) {
                             dispatcher.removeEventListener(type, listener, useCapture);
                         }
                     },
                     dispatchEvent: {
-                        value: function(evt) {
+                        value: function (evt) {
                             dispatcher.dispatchEvent(evt);
                         }
                     },
                     onmessage: {
-                        set: function(aFunction) {
+                        set: function (aFunction) {
                             //clear prevously set event handler
                             if (eventHandler !== null) {
                                 dispatcher.removeEventListener('message', eventHandler, false);
@@ -302,19 +286,19 @@ TODO: File bug about missing strigifier, which Chris had defined.
                             }
                             return eventHandler;
                         },
-                        get: function() {
+                        get: function () {
                             return eventHandler;
                         },
                         enumerable: false,
                         configurable: false
                     },
                     send: {
-                        value: function(data, timestamp) {
-                            return send(self, data, timestamp);
+                        value: function (data, delay) {
+                            return send(self, data, delay);
                         }
                     },
-                    toString: {
-                        value: function toString() {
+                    toJSON: {
+                        value: function toJSON() {
                             var info = {
                                 type: type,
                                 name: name,
@@ -330,20 +314,21 @@ TODO: File bug about missing strigifier, which Chris had defined.
             //http://erlycoder.com/49/javascript-hash-functions-to-convert-string-into-integer-hash-
             function hash(str) {
                 var result = 5381;
-                for (var char, i = 0, l = name.length; i < str.length; i++) {
+                for (var char, i = 0, l = str.length; i < l; i++) {
                     char = str.charCodeAt(i);
                     result = ((result << 5) + result) + char;
                 }
                 return String(result);
             }
 
-            function send(port, data, timestamp) {
-                var delayBeforeSend = (timestamp) ? Math.floor(timestamp - perf.now()) : 0;
+            function send(port, data, delay) {
                 if (port.type === 'input' || !(data) || data.length === 0) {
                     return false;
                 }
-                if (timestamp && (delayBeforeSend > 1)) {
-                    window.setTimeout(send(port, data, timestamp), delayBeforeSend);
+                if (delay && delay > 0) {
+                    window.setTimeout(function () {
+                        send(port, data);
+                    }, delay);
                 } else {
                     midiIO.midiOutLong(data, self.name);
                 }
@@ -364,17 +349,17 @@ TODO: File bug about missing strigifier, which Chris had defined.
         function MIDIAccess() {
             var interfaces = {
                 getInputs: {
-                    value: function() {
+                    value: function () {
                         return midiIO.midiInList.slice(0);
                     }
                 },
                 getOutputs: {
-                    value: function() {
+                    value: function () {
                         return midiIO.midiOutList.slice(0);
                     }
                 },
                 getPortById: {
-                    value: function(id) {
+                    value: function (id) {
                         var ports = this.getInputs().concat(this.getOutputs);
                         id = String(id);
                         for (var i = 0, l = ports.length; i < l; i++) {
@@ -388,6 +373,12 @@ TODO: File bug about missing strigifier, which Chris had defined.
             };
             Object.defineProperties(this, interfaces);
         }
+
+        //once we are allowed, lets get the ports list
+        dispatcher.addEventListener('allowed', function (e) {
+            inputPorts = createPorts(e.data.inputPorts, 'input');
+            outputPorts = createPorts(e.data.outputPorts, 'output');
+        });
         Object.defineProperties(this, interfaces);
     }
 }(window, window.navigator, window.performance));
