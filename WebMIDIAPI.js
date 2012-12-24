c31761e1a2a26a3a687695abd3c367f447eb7dda
Marcos Caceres <marcosscaceres@gmail.com>
2012-12-24 00:40:56 +0000

    fixed issue with performance.now(), moved performance to the top of page.

--- WebMIDIAPI.js
+++ WebMIDIAPI.js
@@ -1,5 +1,73 @@
 /*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:false, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
 /*
+ * performance.now()
+ * Polyfill of: http://www.w3.org/TR/hr-time/
+ *
+ * Copyright (c) 2012 Marcos Caceres
+ * Licensed under the MIT license.
+ */
+(function (exports) {
+    'use strict';
+    var perf = {},
+        props;
+
+    function findNowMethod() {
+        var prefix = 'moz,webkit,opera,ms'.split(','),
+            i = prefix.length,
+            //worst case, we use Date.now()
+            props = {
+                value: timecall(Date.now())
+            };
+
+        function timecall(start) {
+            return function () {
+                return Date.now() - start;
+            };
+        }
+        function methodCall(method) {
+            return function () {
+                return exports.performance[method]();
+            };
+        }
+
+        //seach for vendor prefixed version
+        for (var method; i >= 0; i--) {
+            if ((prefix[i] + 'Now') in exports.performance) {
+                method = prefix[i] + 'Now';
+                props.value = methodCall(method);
+                return props;
+            }
+        }
+
+        //otherwise, try to use connectionStart
+        if ('timing' in exports.performance &&
+            'connectStart' in exports.performance.timing) {
+            //this pretty much approximates performance.now() to the millisecond
+            props.value = timecall(exports.performance.timing.connectStart);
+        }
+        return props;
+    }
+
+    //if already defined, bail
+    if (('performance' in exports) && ('now' in exports.performance)) {
+        return;
+    }
+    //If we have no 'performance' at all, create it
+    if (!('performance' in exports)) {
+        Object.defineProperty(exports, 'performance', {
+            get: function () {
+                return perf;
+            }
+        });
+    }
+    props = findNowMethod();
+    Object.defineProperty(exports.performance, 'now', props);
+
+}(typeof exports === 'object' && exports || this));
+
+
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:false, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
+/*
  * Web MIDI API - Prollyfill
  * https://dvcs.w3.org/hg/audio/raw-file/tip/midi/specification.html
  *
@@ -229,8 +297,25 @@
                         get: function () {
                             return port;
                         }
+                    },
+                    currentTarget:{
+                        get: function(){
+                            return;
+                        }
+                    },
+                    target:{
+                        get: function(){
+                            return;
+                        }
+                    },
+                    srcElement:{
+                        get: function(){
+                            return;
+                        }
                     }
+
                 };
+
             Object.defineProperties(e, interfaces);
             return e;
         }
@@ -410,69 +495,4 @@
     }
 }(window, window.navigator, window.performance));
 
-/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:false, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
-/*
- * performance.now()
- * Polyfill of: http://www.w3.org/TR/hr-time/
- *
- * Copyright (c) 2012 Marcos Caceres
- * Licensed under the MIT license.
- */
-(function (exports) {
-    'use strict';
-    var perf = {},
-        props;
 
-    function findNowMethod() {
-        var prefix = 'moz,webkit,opera,ms'.split(','),
-            i = prefix.length,
-            //worst case, we use Date.now()
-            props = {
-                value: timecall(Date.now())
-            };
-
-        function timecall(start) {
-            return function () {
-                return Date.now() - start;
-            };
-        }
-        function methodCall(method) {
-            return function () {
-                exports.performance[method]();
-            };
-        }
-
-        //seach for vendor prefixed version
-        for (var method; i >= 0; i--) {
-            if ((prefix[i] + 'Now') in exports.performance) {
-                method = prefix[i] + 'Now';
-                props.value = methodCall(method);
-                return props;
-            }
-        }
-
-        //otherwise, try to use connectionStart
-        if ('timing' in exports.performance &&
-            'connectStart' in exports.performance.timing) {
-            //this pretty much approximates performance.now() to the millisecond
-            props.value = timecall(exports.performance.timing.connectStart);
-        }
-        return props;
-    }
-
-    //if already defined, bail
-    if (('performance' in exports) && ('now' in exports.performance)) {
-        return;
-    }
-    //If we have no 'performance' at all, create it
-    if (!('performance' in exports)) {
-        Object.defineProperty(exports, 'performance', {
-            get: function () {
-                return perf;
-            }
-        });
-    }
-    props = findNowMethod();
-    Object.defineProperty(exports.performance, 'now', props);
-
-}(typeof exports === 'object' && exports || this));
